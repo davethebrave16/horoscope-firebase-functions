8cea05434a156e6573c89d7b9cdbdf2ddac6e759
David Rainò <davethebrave160691@gmail.com>
2025-09-04 15:44:47 +0200

    fixes

--- functions/main.py
+++ functions/main.py
@@ -3,7 +3,12 @@
 This file imports all function endpoints to make them available for deployment.
 """
 
+import sys
+import os
 from firebase_admin import initialize_app
+
+# Add the current directory to Python path so we can import src modules
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Import all function endpoints to register them
 from src.api.horoscope import calculate_horoscope
